63294058a26e0e347c9cfd218f0e5799f3195478
M Zhou <contact@mzhou.me>
2018-11-06 11:57:09 -0500

    Initialize projects

---
+++ BattleSnake.Network/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BattleSnake.Network
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BattleSnake.Server/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BattleSnake.Server
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
